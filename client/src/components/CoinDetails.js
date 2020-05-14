186c1f9d30670d74ddb35b4ac9c7c62a9ec0290c
nomad302 <sagar.nargundi@gmail.com>
2020-05-14 19:44:52 +0530

    highchart added

--- client/src/components/CoinDetails.js
+++ client/src/components/CoinDetails.js
@@ -1,19 +1,51 @@
 import React from "react";
+import styled from "styled-components";
 import { connect } from "react-redux";
-import { fetchHistoricalData } from "../actions";
+import { fetchHistoricalData, fetchCoins } from "../actions";
+import Chart from "./Chart";
+
+const ChartGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-template-columns: 1fr 3fr;
+  grid-gap: 15px;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  height: 100px;
+  width: 100px;
+  margin: auto;
+`;
 
 class CoinDetails extends React.Component {
   componentDidMount() {
-    console.log(this.props);
     this.props.fetchHistoricalData(this.props.match.params.name);
   }
-
+  getCurrentCoin() {
+    return this.props.coins.find(
+      (coin) => coin.CoinInfo.Name === this.props.match.params.name
+    );
+  }
   render() {
-    return <div>coindetails : {this.props.coins.length}</div>;
+    return (
+      <ChartGrid>
+        <Image
+          src={`https://www.cryptocompare.com${
+            this.getCurrentCoin().CoinInfo.ImageUrl
+          }`}
+          alt={this.props.match.params.name}
+        />
+        <Chart />
+      </ChartGrid>
+    );
   }
 }
+
 const mapStateToProps = (state) => {
-  return { coins: state.coins };
+  return { coins: state.coins, historical: state.historicalData.Data };
 };
 
-export default connect(mapStateToProps, { fetchHistoricalData })(CoinDetails);
+export default connect(mapStateToProps, { fetchHistoricalData, fetchCoins })(
+  CoinDetails
+);
