930b7f1e648a6f8238fcaedcffebae84249279d2
nomad302 <sagar.nargundi@gmail.com>
2020-05-25 17:48:57 +0530

    serving index.html to express server added

--- index.js
+++ index.js
@@ -41,5 +41,19 @@ app.get("/news", async (req, res) => {
   return res.send(JSON.parse(newsData));
 });
 
+// serving index.html
+if (process.env.NODE_ENV === "production") {
+  // express will serve up production assets
+  // like main.js file or main.css file
+  app.use(express.static("client/build"));
+
+  // express will serve up the index.html file
+  // if it doesnt recognise the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT);
